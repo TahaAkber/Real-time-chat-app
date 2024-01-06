3d15457d6d5cf8627a7bf8dd1d108f10f78d1117
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-01-06 18:50:24 +0500

    Update login.jsx and now we are implementing auth-token

--- my-app/src/pages/login.jsx
+++ my-app/src/pages/login.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { auth, provider } from "../config/firebase";
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+
+//user if we left the page or logout to keep alive we have universalcookies
 function Login() {
   const navigate = useNavigate();
   const signin = async () => {
