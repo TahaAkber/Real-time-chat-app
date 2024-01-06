5b3004c4b95348351e80e92ee8db9d2080b0d984
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-01-06 19:00:13 +0500

    auth token added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cookies from "universal-cookie";
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-        </Routes>
-      </Router>
-    </div>
-  );
+  const cookies = new Cookies();
+  const [isauth, setisauth] = useState(cookies.get("auth-token"));
+  if (!isauth) {
+    return (
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
--- my-app/src/pages/login.jsx
+++ my-app/src/pages/login.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { auth, provider } from "../config/firebase";
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-
+import Cookies from "universal-cookie";
 //user if we left the page or logout to keep alive we have universalcookies
+const cookies = new Cookies();
 function Login() {
   const navigate = useNavigate();
   const signin = async () => {
     const result = await signInWithPopup(auth, provider);
     navigate("/");
-    console.log(result);
+    cookies.set("auth-token", result.user.refreshToken);
   };
   return (
     <div>
