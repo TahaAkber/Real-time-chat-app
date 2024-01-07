76fd8fc89025da13290f7ddf62e062622c70b61b
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-01-08 03:07:16 +0500

    basic app structure complete
    like when you want to login and goto certain page and you type to go tou another page we use this type of structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,32 @@
 import { useState } from "react";
 import "./App.css";
-import Home from "./pages/Home";
+import Home from "./pages/Chat";
 import Login from "./pages/login";
 import { useRef } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Cookies from "universal-cookie";
+import Chat from "./pages/Chat";
 function App() {
   const cookies = new Cookies();
   const [isauth, setisauth] = useState(cookies.get("auth-token"));
   const [room, setroom] = useState(null);
   const roomsetref = useRef(null);
+  // we need to go to directly into chat page without refresh if user is login in this way we should pass setisauth to loginpage as props and set to true
   if (!isauth) {
     return (
       <div className="App">
-        <Home />
-        <Login />
+        {/* <Home /> */}
+        <Login setisauth={setisauth} />
       </div>
     );
   }
   return (
     <div>
       {room ? (
-        <div>chat</div>
+        <div>
+          {" "}
+          <Chat />
+        </div>
       ) : (
         <div>
           <h2>Enter Room Name</h2>
--- my-app/src/pages/login.jsx
+++ my-app/src/pages/login.jsx
@@ -5,12 +5,11 @@ import { useNavigate } from "react-router-dom";
 import Cookies from "universal-cookie";
 //user if we left the page or logout to keep alive we have universalcookies
 const cookies = new Cookies();
-function Login() {
-  const navigate = useNavigate();
+function Login(props) {
   const signin = async () => {
     const result = await signInWithPopup(auth, provider);
-    navigate("/");
     cookies.set("auth-token", result.user.refreshToken);
+    props.setisauth(true);
   };
   return (
     <div>
