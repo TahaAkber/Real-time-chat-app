e4c609088f92944a02748bdee2140500aa7d984d
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-01-07 19:16:58 +0500

    useref functionality is a type of event.target.value to get value from input

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,21 +2,19 @@ import { useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/login";
+import { useRef } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Cookies from "universal-cookie";
 function App() {
   const cookies = new Cookies();
   const [isauth, setisauth] = useState(cookies.get("auth-token"));
   const [room, setroom] = useState(null);
+  const roomsetref = useRef(null);
   if (!isauth) {
     return (
       <div className="App">
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-          </Routes>
-        </Router>
+        <Home />
+        <Login />
       </div>
     );
   }
@@ -27,8 +25,14 @@ function App() {
       ) : (
         <div>
           <h2>Enter Room Name</h2>
-          <input />
-          <button>Enter chat</button>
+          <input ref={roomsetref} />
+          <button
+            onClick={() => {
+              setroom(roomsetref.current.value);
+            }}
+          >
+            Enter chat
+          </button>
         </div>
       )}
     </div>
